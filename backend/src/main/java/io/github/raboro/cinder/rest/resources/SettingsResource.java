a49f6526db90b976303a1be3d8efc5623472226e
Raboro <marius.woerfelreal@gmail.com>
2024-07-27 23:25:28 +0200

    feat(Settings): :sparkles: added patch for Settings

--- backend/src/main/java/io/github/raboro/cinder/rest/resources/SettingsResource.java
+++ backend/src/main/java/io/github/raboro/cinder/rest/resources/SettingsResource.java
@@ -3,11 +3,14 @@ package io.github.raboro.cinder.rest.resources;
 import io.github.raboro.cinder.rest.dto.SettingsDTO;
 import io.github.raboro.cinder.rest.mapper.SettingsMapper;
 import io.github.raboro.cinder.services.SettingsService;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Optional;
 
@@ -27,5 +30,12 @@ public class SettingsResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Optional<SettingsDTO> getSettings() {
         return service.getSettings().map(mapper::toDTO);
+    }
+
+    @PATCH
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public SettingsDTO patchSettings(@RequestBody SettingsDTO dto) {
+        return service.patchSettings(dto);
     }
 }
--- backend/src/main/java/io/github/raboro/cinder/services/SettingsService.java
+++ backend/src/main/java/io/github/raboro/cinder/services/SettingsService.java
@@ -2,24 +2,51 @@ package io.github.raboro.cinder.services;
 
 import io.github.raboro.cinder.dao.SettingsRepository;
 import io.github.raboro.cinder.entities.Settings;
+import io.github.raboro.cinder.rest.dto.SettingsDTO;
+import io.github.raboro.cinder.rest.mapper.CurrencyMapper;
+import io.github.raboro.cinder.rest.mapper.SettingsMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 @Service
 public class SettingsService {
 
     private final SettingsRepository settingsRepository;
+    private final SettingsMapper mapper;
+    private final CurrencyMapper currencyMapper;
 
     @Autowired
-    public SettingsService(SettingsRepository settingsRepository) {
+    public SettingsService(SettingsRepository settingsRepository,
+                           SettingsMapper mapper,
+                           CurrencyMapper currencyMapper) {
         this.settingsRepository = settingsRepository;
+        this.mapper = mapper;
+        this.currencyMapper = currencyMapper;
     }
 
     public Optional<Settings> getSettings() {
         List<Settings> settingsList = settingsRepository.findAll();
         return settingsList.isEmpty() ? Optional.empty() : Optional.of(settingsList.get(0));
+    }
+
+    public SettingsDTO patchSettings(SettingsDTO dto) {
+        return mapper.toDTO(settingsRepository.save(
+                getSettings()
+                        .map(updateSettings(dto))
+                        .orElseGet(() -> mapper.toEntity(dto)))
+        );
+    }
+
+    private Function<Settings, Settings> updateSettings(SettingsDTO dto) {
+        return existingSettings -> {
+            existingSettings.setCurrency(currencyMapper.toEntity(dto.currency()));
+            existingSettings.setMinCost(dto.minCost());
+            existingSettings.setMaxCost(dto.maxCost());
+            return existingSettings;
+        };
     }
 }
