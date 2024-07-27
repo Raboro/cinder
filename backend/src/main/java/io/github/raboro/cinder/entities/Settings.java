b8047ee70be94c2f2c180a5c16bb65aad9df320a
Raboro <marius.woerfelreal@gmail.com>
2024-07-27 23:04:27 +0200

    refactor(Settings): :recycle: replaced wildcard import with specific ones

--- backend/src/main/java/io/github/raboro/cinder/entities/Settings.java
+++ backend/src/main/java/io/github/raboro/cinder/entities/Settings.java
@@ -1,7 +1,12 @@
 package io.github.raboro.cinder.entities;
 
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
