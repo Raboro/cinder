51509740c3fdaaf6014e0af6d8ce91adec35dd65
Raboro <marius.woerfelreal@gmail.com>
2024-07-30 00:42:28 +0200

    test(Conference): ::white_check_mark: added test to get page of conferences with conditions

---
+++ backend/src/test/java/io/github/raboro/cinder/dao/ConferenceRepositoryTest.java
@@ -0,0 +1,99 @@
+package io.github.raboro.cinder.dao;
+
+import io.github.raboro.cinder.entities.Category;
+import io.github.raboro.cinder.entities.Conference;
+import io.github.raboro.cinder.entities.Country;
+import io.github.raboro.cinder.entities.Day;
+import io.github.raboro.cinder.entities.Duration;
+import io.github.raboro.cinder.entities.Location;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class ConferenceRepositoryTest {
+
+    @Autowired
+    private ConferenceRepository repository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private DurationRepository durationRepository;
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    @BeforeEach
+    void setup() {
+        repository.deleteAll();
+        categoryRepository.deleteAll();
+        durationRepository.deleteAll();
+        locationRepository.deleteAll();
+    }
+
+    @Test
+    void emptyConferencesShouldReturnEmptyPage() {
+        Category ai = new Category("AI");
+        categoryRepository.save(ai);
+        Page<Conference> conferences = repository.findByCountryAndCategoriesAndMaxCostAndFuture(
+                Country.GER,
+                List.of(ai),
+                1500F,
+                (int) Calendar.getInstance().getTimeInMillis(),
+                PageRequest.of(0, 3)
+        );
+
+        assertEquals(3, conferences.getSize());
+        assertEquals(0, conferences.get().toList().size());
+    }
+
+    @Test
+    void savedConferenceWithMatchingConditionsShouldBeManpage() throws URISyntaxException, MalformedURLException {
+        Category ai = new Category("AI");
+        categoryRepository.save(ai);
+
+        Day day = new Day(
+                new Date(Calendar.getInstance().getTimeInMillis()),
+                (int) Calendar.getInstance().getTimeInMillis(),
+                (int) Calendar.getInstance().getTimeInMillis() + 100
+        );
+        Duration duration = new Duration(List.of(day), day, day);
+        durationRepository.save(duration);
+        Location location = new Location(Country.GER, "FFF", 10);
+        locationRepository.save(location);
+        Conference conference1 = new Conference(
+                List.of(ai),
+                duration,
+                location,
+                "DevConf",
+                1200F,
+                new URI("https://google.com").toURL()
+        );
+        repository.save(conference1);
+
+        Page<Conference> conferences = repository.findByCountryAndCategoriesAndMaxCostAndFuture(
+                Country.GER,
+                List.of(ai),
+                1500F,
+                (int) Calendar.getInstance().getTimeInMillis() -10000,
+                PageRequest.of(0, 3)
+        );
+        List<Conference> conferencesAsList = conferences.get().toList();
+        assertEquals(1, conferencesAsList.size());
+    }
+
+}
