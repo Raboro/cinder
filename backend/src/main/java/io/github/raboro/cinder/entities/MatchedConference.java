e08c2449457fd3ddb97870fbe862fa2cc7ceef1a
Raboro <marius.woerfelreal@gmail.com>
2024-08-01 19:44:28 +0200

    feat(MatchedConference): :sparkles: added correct constructor

--- backend/src/main/java/io/github/raboro/cinder/entities/MatchedConference.java
+++ backend/src/main/java/io/github/raboro/cinder/entities/MatchedConference.java
@@ -1,19 +1,37 @@
 package io.github.raboro.cinder.entities;
 
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.util.List;
 
 @Entity
 @ToString
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class MatchedConference extends Conference {
     private boolean accepted;
     private boolean past;
+
+    public MatchedConference(List<Category> categories,
+                             Duration duration,
+                             Location location,
+                             String name,
+                             float cost,
+                             String website,
+                             boolean accepted,
+                             boolean past) {
+        setCategories(categories);
+        setDuration(duration);
+        setLocation(location);
+        setName(name);
+        setCost(cost);
+        setWebsite(website);
+        this.accepted = accepted;
+        this.past = past;
+    }
 }
