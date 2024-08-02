92bd0ba75221543b66c0f5131db71a03b9fa1b0a
Raboro <marius.woerfelreal@gmail.com>
2024-08-02 23:04:09 +0200

    feat(ArchivedConference): :sparkles: added findAllWithPage

--- backend/src/main/java/io/github/raboro/cinder/dao/ArchivedConferenceRepository.java
+++ backend/src/main/java/io/github/raboro/cinder/dao/ArchivedConferenceRepository.java
@@ -1,11 +1,17 @@
 package io.github.raboro.cinder.dao;
 
 import io.github.raboro.cinder.entities.ArchivedConference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface ArchivedConferenceRepository extends JpaRepository<ArchivedConference, UUID> {
+
+    @Query("SELECT c from ArchivedConference c")
+    Page<ArchivedConference> findAllWithPage(Pageable pageable);
 }
