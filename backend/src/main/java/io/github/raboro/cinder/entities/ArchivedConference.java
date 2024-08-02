1939a083ba7b3223e415904f9f6654513ae51f11
Raboro <marius.woerfelreal@gmail.com>
2024-08-02 22:44:20 +0200

    feat(MatchedConference): :sparkles: enhanced with annotations and constructor

--- backend/src/main/java/io/github/raboro/cinder/entities/ArchivedConference.java
+++ backend/src/main/java/io/github/raboro/cinder/entities/ArchivedConference.java
@@ -1,7 +1,22 @@
 package io.github.raboro.cinder.entities;
 
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
 
 @Entity
+@ToString
+@Setter
+@Getter
+@NoArgsConstructor
 public class ArchivedConference extends Conference {
+
+    public ArchivedConference(List<Category> categories, Duration duration, Location location, String name, float cost,
+                      String website) {
+        super(categories, duration, location, name, cost, website);
+    }
 }
